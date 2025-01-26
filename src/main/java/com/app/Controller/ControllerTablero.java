8c53337d19b5d274c29dcd046d67b167feff732f
Aletarget <alejomora1205@gmail.com>
2025-01-26 11:23:01 -0500

    Modificaciones generales para el funcionamiento de Composite

--- src/main/java/com/app/Controller/ControllerTablero.java
+++ src/main/java/com/app/Controller/ControllerTablero.java
@@ -14,47 +14,47 @@ import com.app.Tablero.TableroJuego;
 
 public class ControllerTablero {
     public TableroJuego tablero;
-    public ArrayList<Ficha> piezasEquipo;
-    public ArrayList<Ficha> piezasEquipoBlanco;
-    public ArrayList<Ficha> piezasEquipoNegro;
+    public ArrayList<Casilla> piezasEquipo;
+    public ArrayList<Casilla> piezasEquipoBlanco;
+    public ArrayList<Casilla> piezasEquipoNegro;
 
     public ControllerTablero() {
-        tablero = tablero.getInstancia();
+        this.tablero = TableroJuego.getInstancia();
         piezasEquipo = new ArrayList<>();
         piezasEquipoBlanco = new ArrayList<>();
         piezasEquipoNegro = new ArrayList<>();
     }
 
-    public ArrayList<Ficha> fichasBlancas(){
+    public ArrayList<Casilla> fichasBlancas(){
         EquipoBlanco equi_bl = new EquipoBlanco();
         for(int i = 1 ; i <= 8; i++ ){
-            piezasEquipoBlanco.add(new Peon(equi_bl, 2, i));
+            piezasEquipoBlanco.add(new Casilla(new Peon(equi_bl, 2, i)));
         }
-        piezasEquipoBlanco.add(new Torre(equi_bl, 1, 1)); 
-        piezasEquipoBlanco.add(new Torre(equi_bl, 1, 8)); 
-        piezasEquipoBlanco.add(new Caballo(equi_bl, 1, 2));
-        piezasEquipoBlanco.add(new Caballo(equi_bl, 1, 7));
-        piezasEquipoBlanco.add(new Alfil(equi_bl, 1, 3));
-        piezasEquipoBlanco.add(new Alfil(equi_bl, 1, 6));
-        piezasEquipoBlanco.add(new Reina(equi_bl, 1, 4));
-        piezasEquipoBlanco.add(new Rey(equi_bl, 1, 5)); 
+        piezasEquipoBlanco.add(new Casilla(new Torre(equi_bl, 1, 1))); 
+        piezasEquipoBlanco.add(new Casilla(new Torre(equi_bl, 1, 8))); 
+        piezasEquipoBlanco.add(new Casilla(new Caballo(equi_bl, 1, 2)));
+        piezasEquipoBlanco.add(new Casilla(new Caballo(equi_bl, 1, 7)));
+        piezasEquipoBlanco.add(new Casilla(new Alfil(equi_bl, 1, 3)));
+        piezasEquipoBlanco.add(new Casilla(new Alfil(equi_bl, 1, 6)));
+        piezasEquipoBlanco.add(new Casilla(new Reina(equi_bl, 1, 4)));
+        piezasEquipoBlanco.add(new Casilla(new Rey(equi_bl, 1, 5))); 
 
         return piezasEquipoBlanco;
     }
 
-    public ArrayList<Ficha> fichasNegras(){
+    public ArrayList<Casilla> fichasNegras(){
         EquipoNegro equi_ne = new EquipoNegro();
         for(int i = 1 ; i <= 8; i++ ){
-            piezasEquipoBlanco.add(new Peon(equi_ne, 7, i));
+            piezasEquipoBlanco.add(new Casilla(new Peon(equi_ne, 7, i)));
         }
-        piezasEquipoNegro.add(new Torre(equi_ne, 8, 1)); 
-        piezasEquipoNegro.add(new Torre(equi_ne, 8, 8)); 
-        piezasEquipoNegro.add(new Caballo(equi_ne, 8, 2));
-        piezasEquipoNegro.add(new Caballo(equi_ne, 8, 7));
-        piezasEquipoNegro.add(new Alfil(equi_ne, 8, 3));
-        piezasEquipoNegro.add(new Alfil(equi_ne, 8, 6));
-        piezasEquipoNegro.add(new Reina(equi_ne, 8, 4));
-        piezasEquipoNegro.add(new Rey(equi_ne, 8, 5)); 
+        piezasEquipoNegro.add(new Casilla(new Torre(equi_ne, 8, 1))); 
+        piezasEquipoNegro.add(new Casilla(new Torre(equi_ne, 8, 8))); 
+        piezasEquipoNegro.add(new Casilla(new Caballo(equi_ne, 8, 2)));
+        piezasEquipoNegro.add(new Casilla(new Caballo(equi_ne, 8, 7)));
+        piezasEquipoNegro.add(new Casilla(new Alfil(equi_ne, 8, 3)));
+        piezasEquipoNegro.add(new Casilla(new Alfil(equi_ne, 8, 6)));
+        piezasEquipoNegro.add(new Casilla(new Reina(equi_ne, 8, 4)));
+        piezasEquipoNegro.add(new Casilla(new Rey(equi_ne, 8, 5))); 
 
         return piezasEquipoNegro;
     }
@@ -72,8 +72,9 @@ public class ControllerTablero {
             System.out.println("Posición inválida: (" + fila + ", " + columna + ")");
             return;
         }
+        Casilla casilla = new Casilla(ficha);
 
-        tablero.getTablero()[fila - 1][columna - 1] = ficha; // Restar 1 porque las matrices comienzan en 0
+        tablero.getTablero()[fila - 1][columna - 1] = casilla; // Restar 1 porque las matrices comienzan en 0
         System.out.println("Colocando " + ficha.getClass().getSimpleName() + " de color " + ficha.getColor() +
                            " en la posición (" + fila + ", " + columna + ")");
     }
@@ -85,13 +86,13 @@ public class ControllerTablero {
         piezasEquipoNegro = fichasNegras();
     
         // Agregar las piezas blancas al tablero
-    for (Ficha ficha : piezasEquipoBlanco) {
-        colocarFicha(ficha.getPos()[0], ficha.getPos()[1], ficha);
+    for (Casilla ficha : piezasEquipoBlanco) {
+        colocarFicha(ficha.getCasilla().getPos()[0], ficha.getCasilla().getPos()[1], ficha.getCasilla());
     }
 
     // Agregar las piezas negras al tablero
-    for (Ficha ficha : piezasEquipoNegro) {
-        colocarFicha(ficha.getPos()[0], ficha.getPos()[1], ficha);
+    for (Casilla ficha : piezasEquipoNegro) {
+        colocarFicha(ficha.getCasilla().getPos()[0], ficha.getCasilla().getPos()[1], ficha.getCasilla());
     }
     
         // Unir ambas listas en piezasEquipo
