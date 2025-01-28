8c948a7bc780aa966a859c7e7a4abef3401ce80c
Aletarget <alejomora1205@gmail.com>
2025-01-28 00:30:20 -0500

    PRIMER IMPLEMENTACION GRAFICA PARA POSICIONAR LAS FICHAS EN SUS CASILLAS CORRESPONDIENTES

--- src/main/java/com/app/Vista/Tablero.java
+++ src/main/java/com/app/Vista/Tablero.java
@@ -1,15 +1,22 @@
 package com.app.Vista;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import com.app.Controller.ControllerTablero;
 import com.app.Tablero.TableroJuego;
 
 public class Tablero extends JPanel {
     private TableroJuego tableroJuego;
+    private ControllerTablero controller;
+    private ImageIcon image;
+    private JButton[][] imagen;
     public Tablero(){
         setLayout(new GridLayout(8, 8));
         setPreferredSize(new Dimension(800,800));
@@ -18,20 +25,34 @@ public class Tablero extends JPanel {
         Color lightColor = new Color(253, 235, 208);
         Color darkColor = new Color(76,195,105);
 
+        controller = new ControllerTablero();
+        tableroJuego = controller.iniciarJuego();
+        image = new ImageIcon(getClass().getResource(tableroJuego.getTablero()[0][7].getCasilla().getImg()));
+        imagen = new JButton[8][8];
+        imagen[0][7] = new JButton();
+        imagen[0][7].setIcon(image);
+        imagen[0][7].setPreferredSize(new Dimension(74,74));
+        //imagen.setBorderPainted(false); // Quitar el borde del botón
+        imagen[0][7].setFocusPainted(false); // Quitar el enfoque visual
+        imagen[0][7].setContentAreaFilled(false);
+        imagen[0][7].setAlignmentX(CENTER_ALIGNMENT);
+        imagen[0][7].setAlignmentY(CENTER_ALIGNMENT);
         // Crear las casillas del tablero
         for (int fila = 0; fila < 8; fila++) {
             for (int columna = 0; columna < 8; columna++) {
-                JPanel cell = new JPanel();
+                JButton boton = new JButton();
+                JPanel cell = new JPanel(new BorderLayout());
                 // Alternar colores
                 if ((fila + columna) % 2 == 0) {
                     cell.setBackground(lightColor);
                 } else {
                     cell.setBackground(darkColor);
                 }
+                boton = imagen[0][7];
+                cell.add(boton, BorderLayout.CENTER);
                 add(cell); // Agregar la celda al tablero
             }
         }
-
         tableroJuego = TableroJuego.getInstancia();
 
         //Con esta anidacion de ciclos for lo que hacemos es asignarle a cada posicion de {fila,columna} al gridLayout
