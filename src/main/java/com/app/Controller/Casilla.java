7afb803b701f96c040ff697ead5df4f56ee49ebd
Aletarget <alejomora1205@gmail.com>
2025-01-29 17:40:39 -0500

    Agregar como atributo objeto de tipo boton para vincularlo con la vista

--- src/main/java/com/app/Controller/Casilla.java
+++ src/main/java/com/app/Controller/Casilla.java
@@ -1,12 +1,33 @@
 package com.app.Controller;
 
+import java.awt.Dimension;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
 import com.app.Fichas.Ficha;
 
 public class Casilla implements Tablero {
     private Ficha ficha;
-
+    private JButton imagen;
     public Casilla(Ficha ficha){
         this.ficha = ficha;
+        if (ficha == null) {
+            imagen = new JButton();
+            imagen.setIcon(null);
+            imagen.setPreferredSize(new Dimension(100,100));
+            //imagen.setBorderPainted(false); // Quitar el borde del botón
+            imagen.setFocusPainted(false); // Quitar el enfoque visual
+            imagen.setContentAreaFilled(false);
+        }else{
+            imagen = new JButton();
+            ImageIcon image = new ImageIcon(getClass().getResource(ficha.getImg()));
+            imagen.setIcon(image);
+            imagen.setPreferredSize(new Dimension(100,100));
+            //imagen.setBorderPainted(false); // Quitar el borde del botón
+            imagen.setFocusPainted(false); // Quitar el enfoque visual
+            imagen.setContentAreaFilled(false);
+        }
     }
 
     @Override
@@ -20,6 +41,15 @@ public class Casilla implements Tablero {
     @Override
     public void cleanCasilla() {
         this.ficha = null;
+    }
+    public JButton getBoton(){
+        return imagen;
+    }
+    public void limpiarBoton(){
+        imagen.setIcon(null);
+    }
+    public void setBoton(){
+        
     }
     
 }
