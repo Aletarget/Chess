f028e87e67772de30430fba4621201aebaecccbe
Aletarget <alejomora1205@gmail.com>
2025-02-02 01:39:03 -0500

    Reina completa con restricciones de paso por fichas del mismo equipo

--- src/main/java/com/app/Movimientos/MovReina.java
+++ src/main/java/com/app/Movimientos/MovReina.java
@@ -19,34 +19,84 @@ public class MovReina implements Movimientos{
         }
         
         // Direcciones posibles (vertical, horizontal y diagonal)
-        int[][] direcciones = {
-            {1, 0}, {-1, 0}, {0, 1}, {0, -1}, // Movimientos verticales y horizontales
-            {1, 1}, {1, -1}, {-1, 1}, {-1, -1} // Movimientos diagonales
+        int[][] direccionesVH = {
+            {1, 0}, {-1, 0}, {0, 1}, {0, -1} // Movimientos verticales y horizontales
         };
-        
-        // Generar todas las posiciones posibles en cada dirección
-        for (int[] direccion : direcciones) {
-            int filaCurrent = currentPosReina[0];
-            int columnaCurrent = currentPosReina[1];
+        int[][] direccionesDI = {
+            {1, 1}, {1, -1}, {-1, 1}, {-1, -1} // Movimientos En diagonal
+        };
+        if (currentPosReina[0] != posObjetivo[0] && currentPosReina[1] != posObjetivo[1]) {
+            for (int[] direccion : direccionesDI) {
+                int filaCurrent = currentPosReina[0]-1;
+                int columnaCurrent = currentPosReina[1]-1;
+                // Avanzar en la dirección mientras no se salga del tablero\
+                while (true) { // Permitir que el alfil siga moviéndose hasta chocar con algo
+                    filaCurrent += direccion[0];  
+                    columnaCurrent += direccion[1];
             
-            while(true) {
-                // Calcular la nueva posición
-                filaCurrent += direccion[0];
-                columnaCurrent += direccion[1];
-
-                if (filaCurrent == fila && columnaCurrent == columna) { //Comprueba si el movimiento es posible
-                    if (tablero.getTablero()[fila][columna].getCasilla() == null || 
-                        tablero.getTablero()[fila][columna].getCasilla().getColor().equals(valEqContrario)) {
-                        tablero.getTablero()[filaCurrent][columnaCurrent].fillCasilla(ficha, posObjetivo);
-                        tablero.getTablero()[currentPosReina[0]][currentPosReina[1]].cleanCasilla();
-                        return true;
+                    // Verificar si la nueva posición está dentro del tablero
+                    if (columnaCurrent > 7 || columnaCurrent < 0 || filaCurrent > 7 || filaCurrent < 0) {
+                        break; // Salir del bucle si se sale del tablero
+                    }
+                    //Comprueba si el camino hasta el destino esta libre de fichas, si por alguna razon hay una ficha que no deja pasar al alfil
+                    //sea blanca o negra, se quedara quieta ya que el movimiento debe ser el que el usuario desea mas no el que podria llegar a realizar
+                    if (tablero.getTablero()[filaCurrent][columnaCurrent].getCasilla() == null ) {
+                        if (filaCurrent == fila && columnaCurrent == columna) {
+                            tablero.getTablero()[filaCurrent][columnaCurrent].fillCasilla(ficha, posObjetivo);
+                            tablero.getTablero()[currentPosReina[0]-1][currentPosReina[1]-1].cleanCasilla();
+                            return true;
+                        
+                        }
+                    }else if (tablero.getTablero()[filaCurrent][columnaCurrent].getCasilla().getColor().equals(valEqContrario)) {
+                        if (filaCurrent == fila && columnaCurrent == columna) {
+                            tablero.getTablero()[filaCurrent][columnaCurrent].fillCasilla(ficha, posObjetivo);
+                            tablero.getTablero()[currentPosReina[0]-1][currentPosReina[1]-1].cleanCasilla();
+                            return true;
+                        }
+                        break;
+                    }else if (tablero.getTablero()[filaCurrent][columnaCurrent].getCasilla().getColor().equals(tablero.getTablero()[currentPosReina[0]-1][currentPosReina[1]-1].getCasilla().getColor())) {
+                        break;
                     }
                 }
-                // Verificar si la nueva posición está dentro del tablero (límites 1-8)
-                if (fila < 1 || fila > 8 || columna < 1 || columna > 8) {
-                    break; // Salir del bucle si se sale del tablero
+            }
+        }else if(currentPosReina[0] == posObjetivo[0] || currentPosReina[1] == posObjetivo[1]){
+            for (int[] direccion : direccionesVH) {
+                int filaCurrent = currentPosReina[0]-1;
+                int columnaCurrent = currentPosReina[1]-1;
+                System.out.println(direccion[0]+" "+direccion[1]);
+                while(true) {
+                    if (currentPosReina[1] == posObjetivo[1]) {
+                        filaCurrent += direccion[0];  
+                    }else if(currentPosReina[0] == posObjetivo[0]){
+                        columnaCurrent += direccion[1];
+                    }
+            
+                    // Verificar si la nueva posición está dentro del tablero
+                    if (columnaCurrent > 7 || columnaCurrent < 0 || filaCurrent > 7 || filaCurrent < 0) {
+                        break; // Salir del bucle si se sale del tablero
+                    }
+    
+                    System.out.println("Fila: "+ filaCurrent+" Columna: "+ columnaCurrent);
+                    if (tablero.getTablero()[filaCurrent][columnaCurrent].getCasilla() == null ) {
+                        if (filaCurrent == fila && columnaCurrent == columna) {
+                           
+                            tablero.getTablero()[filaCurrent][columnaCurrent].fillCasilla(ficha, posObjetivo);
+                            tablero.getTablero()[currentPosReina[0]-1][currentPosReina[1]-1].cleanCasilla();
+                            return true;
+                        
+                        }
+                    }else if (tablero.getTablero()[filaCurrent][columnaCurrent].getCasilla().getColor().equals(valEqContrario)) {
+                        if (filaCurrent == fila && columnaCurrent == columna) {
+                            tablero.getTablero()[filaCurrent][columnaCurrent].fillCasilla(ficha, posObjetivo);
+                            tablero.getTablero()[currentPosReina[0]-1][currentPosReina[1]-1].cleanCasilla();
+                            return true;
+                        }
+                        break;
+                    }else if (tablero.getTablero()[filaCurrent][columnaCurrent].getCasilla().getColor().equals(tablero.getTablero()[currentPosReina[0]-1][currentPosReina[1]-1].getCasilla().getColor())) {
+                        break;
+                    }
+                    
                 }
-                
             }
         }
         return false;
