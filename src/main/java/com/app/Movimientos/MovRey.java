73874d4a74a449bddfc733bf0a887faf773d73b6
Aletarget <alejomora1205@gmail.com>
2025-02-02 23:13:15 -0500

    Movimientos de rey (Falta validacion de jaques)

--- src/main/java/com/app/Movimientos/MovRey.java
+++ src/main/java/com/app/Movimientos/MovRey.java
@@ -1,7 +1,5 @@
 package com.app.Movimientos;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import com.app.Fichas.Ficha;
 import com.app.Tablero.TableroJuego;
@@ -9,21 +7,31 @@ import com.app.Tablero.TableroJuego;
 public class MovRey implements Movimientos{
     @Override
     public Boolean movimiento(Ficha ficha, TableroJuego tablero, int fila, int columna) {
-        List<int[]> posiblesMovimientos = new ArrayList<>();
+        int[] currentPosRey = ficha.getPos();
+        int[] posObjetivo = new int[]{fila+1,columna+1};
+        String equipo = ficha.getColor();
+        String valEqContrario = equipo.equals("Blanco") ? "Negro" : "Blanco";
         int[][] direcciones = {
             {1, 0}, {-1, 0}, {0, 1}, {0, -1},
             {1, 1}, {1, -1}, {-1, 1}, {-1, -1}
         };
         
         for (int[] direccion : direcciones) {
-            int[] nuevaPos = {
-                ficha.getPos()[0] + direccion[0],
-                ficha.getPos()[1] + direccion[1]
-            };
-            // Verificar que la nueva posición esté dentro de los límites del tablero
-            if (nuevaPos[0] >= 0 && nuevaPos[0] < 8 && nuevaPos[1] >= 0 && nuevaPos[1] < 8) {
-                posiblesMovimientos.add(nuevaPos);
-            }
+            int filaCurrent = currentPosRey[0];
+                int columnaCurrent = currentPosRey[1];
+                
+                filaCurrent += direccion[0];
+                columnaCurrent += direccion[1];
+
+                if (filaCurrent == posObjetivo[0] && columnaCurrent == posObjetivo[1]) { //Comprueba si el movimiento es posible
+                    if (tablero.getTablero()[fila][columna].getCasilla() == null || 
+                        tablero.getTablero()[fila][columna].getCasilla().getColor().equals(valEqContrario)) {
+                            
+                        tablero.getTablero()[filaCurrent-1][columnaCurrent-1].fillCasilla(ficha, posObjetivo);
+                        tablero.getTablero()[currentPosRey[0]-1][currentPosRey[1]-1].cleanCasilla();
+                        return true;
+                    }
+                }
         }
         return true;
     }
